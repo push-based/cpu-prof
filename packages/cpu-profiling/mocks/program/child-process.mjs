7608d7407538a7da470e192a4dce761cd5859a64
Michael <michael.hladky@push-based.io>
2025-05-20 00:10:26 +0200

    update tests

---
+++ packages/cpu-profiling/mocks/program/child-process.mjs
@@ -0,0 +1,9 @@
+console.log(`PID ${process.pid}`);
+
+// Keep the process alive for a short duration to allow CPU profiling
+// Do some CPU work to ensure we get profile data
+let sum = 0;
+for (let i = 0; i < 1000000; i++) {
+    sum += Math.sqrt(i);
+}
+process.exit(0);
