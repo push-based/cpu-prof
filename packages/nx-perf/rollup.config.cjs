1e46161e627b3197d28c67cdef908f25255b6d61
Michael Hladky <michael.hladky@push-based.io>
2025-12-11 16:30:06 +0100

    refactor: debug ci 6

--- packages/nx-perf/rollup.config.cjs
+++ packages/nx-perf/rollup.config.cjs
@@ -3,7 +3,6 @@ const { withNx } = require('@nx/rollup/with-nx');
 module.exports = withNx(
   {
     main: './src/index.ts',
-    additionalEntryPoints: ['./src/bin.ts', './src/postinstall.ts'],
     outputPath: './dist',
     tsConfig: './tsconfig.lib.json',
     compiler: 'swc',
