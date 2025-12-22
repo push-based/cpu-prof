a3c1d120657bb3b11f5a1de33f1b4fec4835319a
Michael Hladky <michael.hladky@push-based.io>
2025-12-22 01:04:20 +0100

    refactor: adjust format ignore

--- testing/test-utils/src/lib/utils/os-agnostic-paths.unit.test.ts
+++ testing/test-utils/src/lib/utils/os-agnostic-paths.unit.test.ts
@@ -30,49 +30,49 @@ describe('osAgnosticPath', () => {
 
     it('should convert a path within the CWD to an OS-agnostic path on Linux/macOS', () => {
       expect(
-        osAgnosticPath(`${unixCwd}/.zod2nx-schma/.zod2nx-schma.config.ts`),
+        osAgnosticPath(`${unixCwd}/.zod2nx-schma/.zod2nx-schma.config.ts`)
       ).toBe('<CWD>/.zod2nx-schma/.zod2nx-schma.config.ts');
     });
 
     it('should return paths outside of CWD on Linux/macOS', () => {
       expect(
-        osAgnosticPath(`${unixCwd}/../.zod2nx-schma/.zod2nx-schma.config.ts`),
+        osAgnosticPath(`${unixCwd}/../.zod2nx-schma/.zod2nx-schma.config.ts`)
       ).toBe('../.zod2nx-schma/.zod2nx-schma.config.ts');
     });
 
     it('should handle absolute paths correctly on Linux/macOS', () => {
       expect(osAgnosticPath('/.zod2nx-schma/.zod2nx-schma.config.ts')).toBe(
-        '/.zod2nx-schma/.zod2nx-schma.config.ts',
+        '/.zod2nx-schma/.zod2nx-schma.config.ts'
       );
     });
 
     it('should handle paths with CWD shorthand "." correctly on Linux/macOS', () => {
       expect(osAgnosticPath('./.zod2nx-schma/.zod2nx-schma.config.ts')).toBe(
-        './.zod2nx-schma/.zod2nx-schma.config.ts',
+        './.zod2nx-schma/.zod2nx-schma.config.ts'
       );
     });
 
     it('should handle relative paths correctly on Linux/macOS', () => {
       expect(
-        osAgnosticPath('../../.zod2nx-schma/.zod2nx-schma.config.ts'),
+        osAgnosticPath('../../.zod2nx-schma/.zod2nx-schma.config.ts')
       ).toBe('../../.zod2nx-schma/.zod2nx-schma.config.ts');
     });
 
     it('should handle path segments correctly on Linux/macOS', () => {
       expect(osAgnosticPath('.zod2nx-schma/.zod2nx-schma.config.ts')).toBe(
-        '.zod2nx-schma/.zod2nx-schma.config.ts',
+        '.zod2nx-schma/.zod2nx-schma.config.ts'
       );
     });
 
     it('should NOT modify already OS-agnostic paths on Linux/macOS', () => {
       expect(
-        osAgnosticPath('<CWD>/.zod2nx-schma/.zod2nx-schma.config.ts'),
+        osAgnosticPath('<CWD>/.zod2nx-schma/.zod2nx-schma.config.ts')
       ).toBe('<CWD>/.zod2nx-schma/.zod2nx-schma.config.ts');
     });
   });
 
   describe('Windows', () => {
-    const windowsCWD = String.raw`D:\users\jerry`;
+    const windowsCWD = 'D:\\users\\jerry';
 
     beforeEach(() => {
       cwdSpy = vi.spyOn(process, 'cwd');
@@ -86,38 +86,38 @@ describe('osAgnosticPath', () => {
     it('should return paths outside of CWD on Windows', () => {
       expect(
         osAgnosticPath(
-          `${windowsCWD}\\..\\.zod2nx-schma\\.zod2nx-schma.config.ts`,
-        ),
+          `${windowsCWD}\\..\\.zod2nx-schma\\.zod2nx-schma.config.ts`
+        )
       ).toBe('../.zod2nx-schma/.zod2nx-schma.config.ts');
     });
 
     it('should convert a path within the CWD to an OS-agnostic path on Windows', () => {
       expect(
-        osAgnosticPath(`${windowsCWD}\\.zod2nx-schma\\.zod2nx-schma.config.ts`),
+        osAgnosticPath(`${windowsCWD}\\.zod2nx-schma\\.zod2nx-schma.config.ts`)
       ).toBe('<CWD>/.zod2nx-schma/.zod2nx-schma.config.ts');
     });
 
     it('should handle absolute paths correctly on Windows', () => {
       expect(
-        osAgnosticPath(String.raw`\.zod2nx-schma\.zod2nx-schma.config.ts`),
+        osAgnosticPath(String.raw`\.zod2nx-schma\.zod2nx-schma.config.ts`)
       ).toBe('/.zod2nx-schma/.zod2nx-schma.config.ts');
     });
 
     it('should handle paths with CWD shorthand "." correctly on Windows', () => {
       expect(
-        osAgnosticPath(String.raw`.\.zod2nx-schma\.zod2nx-schma.config.ts`),
+        osAgnosticPath(String.raw`.\.zod2nx-schma\.zod2nx-schma.config.ts`)
       ).toBe('./.zod2nx-schma/.zod2nx-schma.config.ts');
     });
 
     it('should handle relative paths correctly on Windows', () => {
       expect(
-        osAgnosticPath(String.raw`..\..\.zod2nx-schma\.zod2nx-schma.config.ts`),
+        osAgnosticPath(String.raw`..\..\.zod2nx-schma\.zod2nx-schma.config.ts`)
       ).toBe('../../.zod2nx-schma/.zod2nx-schma.config.ts');
     });
 
     it('should handle path segments correctly on Windows', () => {
       expect(
-        osAgnosticPath(String.raw`.zod2nx-schma\.zod2nx-schma.config.ts`),
+        osAgnosticPath(String.raw`.zod2nx-schma\.zod2nx-schma.config.ts`)
       ).toBe('.zod2nx-schma/.zod2nx-schma.config.ts');
     });
   });
